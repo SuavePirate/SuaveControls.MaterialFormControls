6c941aced3adb7602c0b67588192280948813680
SuavePirate <alexander.dunn@outlook.com>
2018-01-21 18:22:46 -0500

    Progress on validation set up for material entry

--- src/SuaveControls.MaterialForms/MaterialEntry.xaml.cs
+++ src/SuaveControls.MaterialForms/MaterialEntry.xaml.cs
@@ -11,10 +11,13 @@ namespace SuaveControls.MaterialForms
 {
     public partial class MaterialEntry : ContentView
     {
+        #region Events
         public event EventHandler<FocusEventArgs> EntryFocused;
         public event EventHandler<FocusEventArgs> EntryUnfocused;
         public event EventHandler<TextChangedEventArgs> TextChanged;
+        #endregion
 
+        #region Bindable Properties
         public static BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(MaterialEntry), defaultBindingMode: BindingMode.TwoWay);
         public static BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(MaterialEntry), defaultBindingMode: BindingMode.TwoWay, propertyChanged: (bindable, oldVal, newval) =>
         {
@@ -34,6 +37,55 @@ namespace SuaveControls.MaterialForms
             matEntry.EntryField.Keyboard = (Keyboard)newVal;
         });
         public static BindableProperty AccentColorProperty = BindableProperty.Create(nameof(AccentColor), typeof(Color), typeof(MaterialEntry), defaultValue: Color.Accent);
+        public static BindableProperty InvalidColorProperty = BindableProperty.Create(nameof(InvalidColor), typeof(Color), typeof(MaterialEntry), Color.Red, propertyChanged: (bindable, oldVal, newVal) =>
+        {
+            var matEntry = (MaterialEntry)bindable;
+            matEntry.UpdateValidation();
+        });
+        public static BindableProperty DefaultColorProperty = BindableProperty.Create(nameof(DefaultColor), typeof(Color), typeof(MaterialEntry), Color.Gray);
+        public static BindableProperty IsValidProperty = BindableProperty.Create(nameof(IsValid), typeof(bool), typeof(MaterialEntry), true, propertyChanged: (bindable, oldVal, newVal) =>
+        {
+            var matEntry = (MaterialEntry)bindable;
+            matEntry.UpdateValidation();
+        });
+        #endregion
+
+        #region Public Properties
+
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(IsValidProperty);
+            }
+            set
+            {
+                SetValue(IsValidProperty, value);
+            }
+        }
+        public Color DefaultColor
+        {
+            get
+            {
+                return (Color)GetValue(DefaultColorProperty);
+            }
+            set
+            {
+                SetValue(DefaultColorProperty, value);
+            }
+        }
+        public Color InvalidColor
+        {
+            get
+            {
+                return (Color)GetValue(InvalidColorProperty);
+            }
+            set
+            {
+                SetValue(InvalidColorProperty, value);
+            }
+        }
+
         public Color AccentColor
         {
             get
@@ -91,11 +143,14 @@ namespace SuaveControls.MaterialForms
                 SetValue(PlaceholderProperty, value);
             }
         }
+
+#endregion
+
         public MaterialEntry()
         {
             InitializeComponent();
             EntryField.BindingContext = this;
-            EntryField.TextChanged += async (s, a) =>
+            EntryField.TextChanged += (s, a) =>
             {
                 TextChanged?.Invoke(s, a);
             };
@@ -120,9 +175,13 @@ namespace SuaveControls.MaterialForms
             };
         }
 
+        /// <summary>
+        /// Calculates the layout when unfocused. Includes running the animation to update the bottom border color and the floating label
+        /// </summary>
+        /// <returns>The layout unfocused.</returns>
         private async Task CalculateLayoutUnfocused()
         {
-            HiddenLabel.TextColor = Color.Gray;
+            HiddenLabel.TextColor = DefaultColor;
             if (string.IsNullOrEmpty(EntryField.Text))
             {
                 // animate both at the same time
@@ -140,6 +199,9 @@ namespace SuaveControls.MaterialForms
             }
         }
 
+        /// <summary>
+        /// Calculates the layout when focused. Includes running the animation to update the bottom border color and the floating label
+        /// </summary>
         private async Task CalculateLayoutFocused()
         {
             HiddenLabel.IsVisible = true;
@@ -161,5 +223,31 @@ namespace SuaveControls.MaterialForms
             }
         }
 
+        /// <summary>
+        /// Updates view based on validation state
+        /// </summary>
+        private void UpdateValidation()
+        {
+            if(IsValid)
+            {
+
+                BottomBorder.Color = DefaultColor;
+                HiddenBottomBorder.Color = AccentColor;
+                if(IsFocused)
+                {
+                    HiddenLabel.TextColor = AccentColor;
+                }
+                else
+                {
+                    HiddenLabel.TextColor = DefaultColor;
+                }
+            }
+            else
+            {
+                BottomBorder.Color = InvalidColor;
+                HiddenBottomBorder.Color = InvalidColor;
+                HiddenLabel.TextColor = InvalidColor;
+            }
+        }
     }
 }
